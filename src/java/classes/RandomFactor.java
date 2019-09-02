b60fbaba9e88b5f258dc471dd7df4ae8ba7e068c
David Sodergren <david.sodergren94@gmail.com>
2019-09-02 13:48:34 +0200

    sessionbeans created

--- src/java/classes/RandomFactor.java
+++ src/java/classes/RandomFactor.java
@@ -1,8 +1,28 @@
-
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package classes;
 
-public class RandomFactor {
+import javax.inject.Named;
+import javax.enterprise.context.SessionScoped;
+import java.io.Serializable;
 
+/**
+ *
+ * @author powkn
+ */
+@Named(value = "randomFactor")
+@SessionScoped
+public class RandomFactor implements Serializable {
+
+    /**
+     * Creates a new instance of RandomFactor
+     */
+    public RandomFactor() {
+    }
+    
     public int getRandomNumber() {
         return (int) (Math.random() * 10) + 1;
     }
--- src/java/classes/Translator.java
+++ src/java/classes/Translator.java
@@ -1,6 +1,26 @@
-
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package classes;
 
-public class Translator {
+import javax.inject.Named;
+import javax.enterprise.context.SessionScoped;
+import java.io.Serializable;
 
+/**
+ *
+ * @author powkn
+ */
+@Named(value = "translator")
+@SessionScoped
+public class Translator implements Serializable {
+
+    /**
+     * Creates a new instance of Translator
+     */
+    public Translator() {
+    }
+    
 }
